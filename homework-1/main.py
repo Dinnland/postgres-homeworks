99f8cc40b4aab593d9d4abdf8c4074df8c0249aa
dinar mingazov <dinarek2013@gmail.com>
2023-06-12 19:10:05 +0300

    1. Созданы три таблицы:     - `employees`     - `customers`     - `orders` 2. установлены связи между ними Sql-код для создания таблиц скопирован в файл `create_tables 3.  Написан скрипт в `main.py`, который заполняет созданные таблицы данными из `north_data`

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,42 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+employees_data = "./north_data/employees_data.csv"
+customers_data = "./north_data/customers_data.csv"
+orders_data = "./north_data/orders_data.csv"
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="9999")
+try:
+    with conn:
+        with conn.cursor() as cur:
+
+            with open(customers_data, newline='', encoding='utf-8') as csvfile:
+                read = csv.reader(csvfile, delimiter=',', quotechar='"')
+                next(read)
+                for row in read:
+                    cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', row)
+
+            with open(employees_data, newline='', encoding='utf-8') as csvfile:
+                i = 0
+                read = csv.reader(csvfile, delimiter=',', quotechar='"')
+                next(read)
+                for row in read:
+                    i += 1
+                    cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", [i] + row)
+
+            with open('north_data/orders_data.csv', newline='', encoding='utf-8') as csvfile:
+                read = csv.reader(csvfile, delimiter=',', quotechar='"')
+                next(read)
+                for row in read:
+                    cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", row)
+
+finally:
+    conn.close()
+
+
+
